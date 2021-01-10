12d156161818efb5ce66970c4cc5c853a3dfabac
Luisa ChirilaMyClass <luisalouie@yahoo.com>
2021-01-10 23:33:11 +0200

    Cars

--- Pages/Cars/Index.cshtml.cs
+++ Pages/Cars/Index.cshtml.cs
@@ -10,31 +10,38 @@ using Chirila_Luisa_Lab9.Models;
 
 namespace Chirila_Luisa_Lab9.Pages.Cars
 {
-    public IndexModel(Chirila_Luisa_Lab9.Data.Chirila_Luisa_Lab9Context context)
+    public class IndexModel: PageModel
     {
-        _context = context;
-    }
-    public IList<Car> Car { get; set; }
-    public CarData CarD { get; set; }
-    public int CarID { get; set; }
-    public int CategoryID { get; set; }
-    public async Task OnGetAsync(int? id, int? categoryID)
-    {
-        CarD = new CarData();
-
-        CarD.Cars = await _context.Car
-        .Include(b => b.Manufacture)
-        .Include(b => b.CarCategories)
-        .ThenInclude(b => b.Category)
-        .AsNoTracking()
-        .OrderBy(b => b.Name)
-        .ToListAsync();
-        if (id != null)
+        private readonly Chirila_Luisa_Lab9.Data.Chirila_Luisa_Lab9Context 
+            _context;
+        public IndexModel(Chirila_Luisa_Lab9.Data.Chirila_Luisa_Lab9Context context)
         {
-            CarID = id.Value;
-            Car car = CarD.Cars
-            .Where(i => i.ID == id.Value).Single();
-            CarD.Categories = car.CarCategories.Select(s => s.Category);
+            _context = context;
+        }
+        
+    
+    public IList<Car> Car { get; set; }
+        public CarData CarD { get; set; }
+        public int CarID { get; set; }
+        public int CategoryID { get; set; }
+        public async Task OnGetAsync(int? id, int? categoryID)
+        {
+            CarD = new CarData();
+
+            CarD.Cars = await _context.Car
+            .Include(b => b.Manufacture)
+            .Include(b => b.CarCategories)
+            .ThenInclude(b => b.Category)
+            .AsNoTracking()
+            .OrderBy(b => b.Name)
+            .ToListAsync();
+            if (id != null)
+            {
+                CarID = id.Value;
+                Car car = CarD.Cars
+                .Where(i => i.ID == id.Value).Single();
+                CarD.Categories = car.CarCategories.Select(s => s.Category);
+            }
         }
     }
 }
